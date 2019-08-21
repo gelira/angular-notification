0b1289fb9801ecc921ca622d7cf1f8183bb05aec
Geraldo Eloi <geraldo.eloi11@gmail.com>
2019-08-21 09:49:15 -0300

    Lançamento das notificações

--- src/app/components/container/container.component.ts
+++ src/app/components/container/container.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { NotificationComponent } from '../notification/notification.component';
 
 @Component({
@@ -9,14 +9,32 @@ import { NotificationComponent } from '../notification/notification.component';
   styleUrls: ['./container.component.css']
 })
 export class ContainerComponent implements OnInit {
+  private config: MatDialogConfig = {
+    width: '400px',
+    hasBackdrop: false,
+    position: {
+      bottom: '20px',
+      left: '20px'
+    }
+  };
 
   constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
   }
 
+  showSuccess() {
+    this.config.panelClass = 'notification-success';
+    this.showNotification();
+  }
+
+  showError() {
+    this.config.panelClass = 'notification-error';
+    this.showNotification();
+  }
+
   showNotification() {
-    const notification = this.dialog.open(NotificationComponent);
+    const notification = this.dialog.open(NotificationComponent, this.config);
     const contador = setTimeout(() => {
       console.log('TIMEOUT');
       notification.close();
