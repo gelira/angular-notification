40e585098de1246bfbf3dfe5931dbed81226e9f3
Geraldo Eloi <geraldo.eloi11@gmail.com>
2019-08-21 05:44:09 -0300

    componente container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialDependenciasModule } from './material-dependencias.module';
+import { ContainerComponent } from './components/container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
